ef84138114aab4621aba6f29d83b52afb36937db
Yogesh Yadav <yogesh.yadav@flywheeldigital.com>
2024-09-26 12:20:09 +0530

    "Initial Commit"

---
+++ ApiGatewayService/src/test/java/com/theMerchantsGuild/ApiGatewayService/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theMerchantsGuild.ApiGatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ PlayerService/src/main/java/com/theMerchantsGuild/PlayerService/PlayerServiceApplication.java
@@ -0,0 +1,16 @@
+package com.theMerchantsGuild.PlayerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class PlayerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerServiceApplication.class, args);
+		System.out.println("-----------------Deepak-------------------------------------------");
+	}
+
+}
